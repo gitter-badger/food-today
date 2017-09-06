e33682033dd5523128d2e98184394ed74f1cc96a
Stadler <mstzo@web.de>
2017-09-06 11:31:20 +0200

    converted java to kotlin, changed sdk build version

---
+++ app/src/androidTest/java/com/example/ghostmst/foodtoday/ExampleInstrumentedTest.kt
@@ -0,0 +1,25 @@
+package com.example.ghostmst.foodtoday
+
+import android.content.Context
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    @Throws(Exception::class)
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+        assertEquals("com.example.ghostmst.foodtoday", appContext.getPackageName())
+    }
+}
